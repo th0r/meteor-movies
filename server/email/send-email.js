b25be3efcb86223dc48ae140f68d0fa9c3794c4f
th0r <th0r@dudu.com>
2013-05-15 11:03:52 +0400

    Emails about new movies

--- server/email/send-email.js
+++ server/email/send-email.js
@@ -1,14 +1,9 @@
 var TO = 'grunin.ya@yandex.ru',
     FROM = 'movies@grunin-ya.ru',
     
-    TMPL_DIR = 'server/email/templates',
-    TMPL_EXT = '.html',
     SUBJECT_TMPL_SUFFIX = '-subject',
     BODY_TMPL_SUFFIX = '-body',
-    
-    fs = Npm.require('fs'),
-    path = Npm.require('path'),
-    Handlebars = Npm.require('handlebars');
+    templates = Handlebars.templates;
 
 function getTemplate(tmplName) {
     var tmplFile = path.resolve(TMPL_DIR, tmplName + TMPL_EXT),
@@ -22,8 +17,8 @@ App.sendEmail = function (messageTmplName, tmplData) {
         var message = {
             from: FROM,
             to: TO,
-            subject: getTemplate(messageTmplName + SUBJECT_TMPL_SUFFIX)(tmplData),
-            html: getTemplate(messageTmplName + BODY_TMPL_SUFFIX)(tmplData)
+            subject: templates[messageTmplName + SUBJECT_TMPL_SUFFIX](tmplData),
+            html: templates[messageTmplName + BODY_TMPL_SUFFIX](tmplData)
         };
 
         Email.send(message);
