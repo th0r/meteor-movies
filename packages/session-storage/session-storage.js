2465a488227553df307efd1464b68efcd6421430
th0r <th0r@dudu.com>
2013-05-30 12:00:56 +0400

    Added custom package Session-Storage

--- packages/session-storage/session-storage.js
+++ packages/session-storage/session-storage.js
@@ -12,20 +12,20 @@ Session.set = function (key, value, useLocalStorage) {
 
 Session.get = function (key, useLocalStorage) {
     var result = _get.apply(Session, arguments);
-    
+
     if (result === undefined && useLocalStorage) {
         result = store.get(key);
         if (result !== undefined) {
             _set.apply(Session, [key, result]);
         }
     }
-    
+
     return result;
 };
 
 Session.setDefault = function (key, value, useLocalStorage) {
     var result = Session.get(key, useLocalStorage);
-    
+
     if (result === undefined) {
         Session.set(key, value, useLocalStorage);
     }
